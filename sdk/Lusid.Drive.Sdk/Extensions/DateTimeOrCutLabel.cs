06fd7fce01ded98695bc42b2f23be36f6d4319b9
Concourse <concourse@finbourne.com>
2024-01-18 18:12:24 +0000

    Updates for v2.0.8

--- sdk/Lusid.Drive.Sdk/Extensions/DateTimeOrCutLabel.cs
+++ sdk/Lusid.Drive.Sdk/Extensions/DateTimeOrCutLabel.cs
@@ -68,6 +68,10 @@ namespace Lusid.Drive.Sdk
         /// </summary>
         public static implicit operator DateTimeOrCutLabel(string cutLabel)
         {
+            if (cutLabel == null)
+            {
+                return default(DateTimeOrCutLabel);
+            }
             return new DateTimeOrCutLabel(cutLabel);
         }
 
